72d9e18c73e39311d8daa7b21bed0545a44f1e14
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-04-05 17:28:36 +0100

    updating model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :lists
+  has_many :items, through: :lists
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
