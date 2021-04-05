fbc172bae99995b6cc2fbfad49335494997d7d11
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-04-05 17:25:16 +0100

    adding seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Running seed..."
+
+puts "Destroying Items table..."
+Item.destroy_all
+
+puts "Destroying Lists table..."
+List.destroy_all
+
+puts "Destroying User table..."
+User.destroy_all
+
+puts "Creating users..."
+jeremaia = User.create!(first_name: 'Jeremaia', last_name: 'Harrison', email: 'jeremaia@example.com', password: '123456')
+tania = User.create!(first_name: 'Tania', last_name: 'Fernandes', email: 'tania@example.com', password: '123456')
+
+puts "Creating Jeremaia's list..."
+list = List.create!(
+  owner: jeremaia.first_name,
+  title: "Jeremaia's list",
+  user_id: jeremaia.id
+)
+
+puts "Adding items to Jeremaia's list..."
+item = Item.create!(
+  item_name: "Tropicfeel",
+  description: "A backpack and wardrobe system",
+  price: 192,
+  link: "www.titemropicfeel.com",
+  buyer: tania.id,
+  list_id: list.id,
+  user_id: jeremaia.id
+)
+
+puts "Creating tania's list..."
+list = List.create!(
+  owner: tania.first_name,
+  title: "Tania's list",
+  user_id: tania.id
+)
+
+puts "Adding items to tania's list..."
+Item.create!(
+  item_name: "Webcam",
+  description: "Logitech C920s Webcam, Black",
+  price: 84.99,
+  link: "www.johnlewis.com",
+  buyer: jeremaia.id,
+  list_id: list.id,
+  user_id: tania.id
+)
+
+
+puts "Seed complete 🌱"
+
