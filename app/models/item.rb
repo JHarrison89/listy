cc6d11e269c69c753db7a0179d0f057996c2b449
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-04-09 11:33:49 +0100

    ensuring all shopping links are absolute

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :list
   belongs_to :user, optional: true
+
+  before_save :smart_add_url_protocol
+
+  protected
+
+  def smart_add_url_protocol
+    unless link[/\Ahttp:\/\//] || link[/\Ahttps:\/\//]
+      self.link = "http://#{link}"
+    end
+  end
 end
