485c0693f8b3d2fda66fb4228401be93fcf80c9a
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-04-13 15:15:28 +0100

    jquery workingon edit.js.erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
+// import $ from 'jquery';
+
+// global.$ = jQuery;
+
+
+
+
 
 console.log('Hello from application.js')
 
@@ -26,6 +32,8 @@ console.log('Hello from application.js')
 
 // External imports
 import "bootstrap";
+// import $ from 'jquery';
+// global.$ = jQuery;
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
